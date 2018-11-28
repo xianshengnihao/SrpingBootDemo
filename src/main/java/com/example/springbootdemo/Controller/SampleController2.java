1a075ea11afa822da272e1bc1287888346f26b1a
郝维格 <979955176@qq.com>
2018-11-28 18:28:52 +0800

    增加注释 修改配置文件

--- src/main/java/com/example/springbootdemo/Controller/SampleController2.java
+++ src/main/java/com/example/springbootdemo/Controller/SampleController2.java
@@ -10,8 +10,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @EnableAutoConfiguration
 public class SampleController2 {
     @RequestMapping("/html")
+    /**
+     * 作用在方法上@ResponseBody 表示该方法的返回结果直接写入 HTTP response body 中，一般在异步获取数据时使用【也就是AJAX】，
+     * 在使用 @RequestMapping后，返回值通常解析为跳转路径，但是加上 @ResponseBody 后返回结果不会被解析为跳转路径，而是直接写入
+     * HTTP response body 中。 比如异步获取 json 数据，加上 @ResponseBody 后，会直接返回 json 数据。@RequestBody 将 HTTP
+     * 请求正文插入方法中，使用适合的 HttpMessageConverter 将请求体写入某个对象。如果返回到html页面则不需要加@ResponseBody
+     */
     @ResponseBody
     String HelloSpringBoot(){
-        return "/index.html";
+//      return "/index.html";
+        return "HelloSpringBoot";
     }
 }
