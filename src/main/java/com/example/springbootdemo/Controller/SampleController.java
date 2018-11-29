25d7b2fcc09d7ddc7cb7f495a8be4057de4e7d2a
郝维格 <979955176@qq.com>
2018-11-29 18:03:07 +0800

    fremarker提交

--- src/main/java/com/example/springbootdemo/Controller/SampleController.java
+++ src/main/java/com/example/springbootdemo/Controller/SampleController.java
@@ -15,7 +15,6 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping(value = "/hello")
-@EnableAutoConfiguration
 public class SampleController {
 
     /**
--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -3,6 +3,14 @@ package com.example.springbootdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1、@EnableAutoConfiguration 默认扫描当前类中的Controller，如要扫描其他包下的Controller
+ *    需配合注解@ComponentScan("com.example.springboodtemo")来使用
+ *    @ComponentScan扫多个包 ：@ComponentScan(basePaPackages={"","",""})
+ *    缺点：当大量Controller包时写太多麻烦。
+ * 2、@SpringBootApplication自动扫描当前包和当前包的同级包，包括统计包下的子包。
+ *   缺点：影响性能
+ */
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
